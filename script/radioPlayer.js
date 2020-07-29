a5ecafba16dc72052a0fada6fcbe140e6e7c69eb
Vova <vovik142@ukr.net>
2020-07-29 17:15:28 +0300

    third

--- script/radioPlayer.js
+++ script/radioPlayer.js
@@ -1,4 +1,69 @@
 export const radioPlayerInit = () => {
-    console.log ('RADIO INIT');
-}
+    // radio
+    // radio-cover__img
+    // radio-header
+    // radio-item
+    // radio-stop 
+
+    const radio = document.querySelector('.radio'),
+    radioCoverImg = document.querySelector('.radio-cover__img'),
+    radioHeaderBig = document.querySelector('.radio-header__big'),
+    radioNavigation = document.querySelector('.radio-navigation'),
+    radioItem = document.querySelectorAll('.radio-item'),
+    audioVolume = document.querySelector('.audio-volume'),
+    radioStop = document.querySelector('.radio-stop');
+
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    }
+
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parrent = target.closest('.radio-item');
+        selectItem(parrent); 
+        
+        const title = parrent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title;
+
+        const urlImg = parrent.querySelector('.radio-img').src;
+        radioCoverImg.src = urlImg;
+
+        radioStop.disabled = false;
+        audio.src = target.dataset.radioStantion;
+        audio.play();
+        changeIconPlay();
+    });
+      radioStop.addEventListener('click', () => {
+          if (audio.paused) {
+              audio.play();
+          } else {
+              audio.pause();
+
+          }
+          changeIconPlay();
+      })
+
+      audioVolume.addEventListener('input', function(){
+        audio.volume = parseFloat(this.value / 100);
+    }, false);
+};
 
